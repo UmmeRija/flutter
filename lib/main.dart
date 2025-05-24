dd1bcbfe492049638067412d74f8417eb7f258e5
UmmeRija <ummerijababar@gmail.com>
2025-05-24 11:04:14 +0500

    git commit -m "forms"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/bottom_bar.dart';
 import 'package:my_app/image_work.dart';
@@ -12,6 +13,7 @@ import 'package:my_app/to_do_list/to_do_controller.dart';
 import 'package:my_app/to_do_list/to_do_screen.dart';
 import 'package:provider/provider.dart';
 import 'app_bar.dart';
+import 'firebase_options.dart';
 import 'first_practice.dart';
 // import 'whatsapp_screen.dart';
 import 'formstry.dart';
@@ -20,7 +22,11 @@ import 'date_and_time.dart';
 import 'navigator.dart';
 import 'sirka_code.dart';
 import 'screen/first_screen.dart';
-void main(){
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
--- lib/practice_app/login_screen.dart
+++ lib/practice_app/login_screen.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/practice_app/forgot_pasword.dart';
+import 'package:my_app/practice_app/register_screen.dart';
 
 import 'app_colors.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailControler = TextEditingController();
+  final TextEditingController paswordControler = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,44 +24,87 @@ class LoginScreen extends StatelessWidget {
         height: double.infinity,
         width: double.infinity,
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 20,
-            children: [
-              Text("Login Form" , style: TextStyle(color: AppColors.textColor , fontSize: 20),
+          child: Form(
+            key: _key,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 20,
+              children: [
+                Text("Login Form" , style: TextStyle(color: AppColors.textColor , fontSize: 20),
 
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(),
-                  hintText: "Email" , hintStyle: TextStyle(color: AppColors.textColor,
-                fontSize: 20),
-                  prefixIcon: Icon(Icons.email , color: AppColors.textColor,)
-                ),),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(),
-                  hintText: "Pasword" , hintStyle: TextStyle(color: AppColors.textColor,
-                    fontSize: 20),
-                  prefixIcon: Icon(Icons.lock , color: AppColors.textColor)
-                ),),
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.primary,
-                  ) ,
-                  onPressed: (){},
-                  child:Center(child: Text("Login" , style: TextStyle(color: AppColors.textColor,))),
-              )
-            ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: emailControler,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Pasword field is empty";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
+                    hintText: "Email" , hintStyle: TextStyle(color: AppColors.textColor,
+                  fontSize: 20),
+                    prefixIcon: Icon(Icons.email , color: AppColors.textColor,)
+                  ),),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: paswordControler,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Pasword field is empty";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
+                    hintText: "Pasword" , hintStyle: TextStyle(color: AppColors.textColor,
+                      fontSize: 20),
+                    prefixIcon: Icon(Icons.lock , color: AppColors.textColor)
+                  ),),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CupertinoButton(child: Text("Forget pasword"), onPressed:(){
+                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return ForgotPasword();
+                      }));
+                    })
+                  ],
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                    ) ,
+                    onPressed: (){
+                      if(_key.currentState!.validate());
+                    },
+                    child:Center(child: Text("Login" , style: TextStyle(color: AppColors.textColor,))),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't Have An Account?"),
+                    CupertinoButton(child: Text("Register") , onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return RegisterScreen();
+                      }));
+                    })
+                  ],
+                ),
 
+              ],
+
+            ),
           ),
         ),
       ),
